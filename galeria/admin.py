cce35d76a4a9b7f46df93c46cbde66bda36665f8
Vicente Elias <vicente.e.paula@gmail.com>
2024-09-06 21:52:59 -0300

    manipulando o banco de dados

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda")
+    list_display_links = ("id", "nome")
+    search_fields = ("nome",)
+    list_filter = ("categoria", )
+    list_per_page = 10
+    
+admin.site.register(Fotografia, ListandoFotografias)
